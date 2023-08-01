d739fd082efdd39421b07ae66816c67d703388d0
KatyW <ktabe13@gmail.com>
2023-07-31 22:30:43 -0500

    adding delete methods

--- db/query.sql
+++
@@ -1,4 +0,0 @@
-UPDATE employee
-SET employee.role_id = 3
-INNER JOIN roles ON employee.ro_id = roles.id
-WHERE employee.first_name = 'Lisa' and employee.last_name = "Tomlin";
--- index.js
+++ index.js
@@ -22,12 +22,14 @@ async function init() {
                 type: 'list',
                 message: 'What would you like to do?',
                 choices: ["1. View All Departments", "2. View All Rows", "3. View All Employees", "4. Add A Department",
-                    "5. Add A Role", "6. Add An Employee", "7. Update An Employee Role", "8. Quit"],
+                    "5. Add A Role", "6. Add An Employee", "7. Update An Employee Role", "8. Delete Department", 
+                    "9. Delete Role", "10. Delete Employee", "11. Quit"],
                 name: 'action',
             },
         ])
 
-    switch (response.action[0]) {
+    const option =response.action.split('.')[0];
+    switch (option) {
         case "1":
             ViewAll("department");
             break;
@@ -50,6 +52,16 @@ async function init() {
             UpdateEmployee();
             break;
         case "8":
+            DeleteDepartment();
+            break;
+        case "9":
+            DeleteRole();
+            break;
+        case "10":
+            console.log("11");
+            DeleteEmployee();
+            break;
+        case "11":
             console.log("Bye");
             process.exit(3);
         default:
@@ -73,6 +85,76 @@ async function ViewAll(table_db) {
     setTimeout(() => { init() }, 1000);
 }
 
+async function DeleteDepartment() {
+    const sql = `DELETE FROM department WHERE department_name = ?`;
+    const departments = await getAllDepartments();
+
+    const response = await inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'Please select the department to delete: ',
+                choices: departments,
+                name: 'department',
+            },
+        ])
+
+    await db.query(sql, [response.department.trim()], function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+    console.log(response.department + " deleted from database.");
+    init();
+}
+
+async function DeleteRole() {
+    const sql = `DELETE FROM roles WHERE title = ?`;
+    const roles = await getAllRoles();
+
+    const response = await inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'Please select the role to delete: ',
+                choices: roles,
+                name: 'role',
+            },
+        ])
+
+    await db.query(sql, [response.role], function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+    console.log(response.role + " deleted from database.");
+    init();
+}
+
+async function DeleteEmployee() {
+    console.log("here");
+    const sql = `DELETE FROM employee WHERE first_name = ? and last_name = ?`;
+    const employees = await getAllEmployees();
+
+    const response = await inquirer
+        .prompt([
+            {
+                type: 'list',
+                message: 'Please select the employee to delete: ',
+                choices: employees,
+                name: 'employee',
+            },
+        ])
+
+    await db.query(sql, [[response.employee.split(' ')[0]],[response.employee.split(' ')[1]]], function (err) {
+        if (err) {
+            console.log(err);
+        }
+    });
+    console.log(response.employee + " deleted from database.");
+    init();
+}
+
 //inserts new department to department db table
 async function AddDepartment() {
     const sql = 'INSERT INTO `department` (department_name) VALUES (?)';
@@ -88,13 +170,13 @@ async function AddDepartment() {
         if (err) {
             console.log(err);
         }
-        console.log(response.department + " added to database.");
     });
-    setTimeout(() => { init() }, 1000);
+    console.log(response.department + " added to database.");
+    init();
 }
 
 async function AddRole() {
-    const sql = 'INSERT INTO `role` (title, salary, department_id) VALUES (?)';
+    const sql = `INSERT INTO roles (title, salary, department_id) VALUES (?)`;
     const departments = await getAllDepartments();
 
     const response = await inquirer
